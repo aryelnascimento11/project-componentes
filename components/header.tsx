9be4f5191b9b6df5611373cb0112ce499ec4528a
Aryel <126723967+aryelnascimento11@users.noreply.github.com>
2023-09-04 14:15:29 -0300

    create.desafio-2

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href=""></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
