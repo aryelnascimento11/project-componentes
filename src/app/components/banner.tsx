7f8aec934f3a82014453adcf9af9814884a1017c
aryelnascimento11 <aryeln897@gmail.com>
2023-09-12 22:54:26 -0300

    style

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,15 +1,12 @@
-export default function Banner(){
-    return(
-<>
- <section className="flex place-items-center h-[50vw] mt-5 bg-cover bg-[url('https://img.freepik.com/vetores-gratis/laptop-com-icone-de-codigo-isometrico-de-programa-desenvolvimento-de-software-e-aplicacoes-de-programacao-neon-escuro_39422-971.jpg')]">
-  <div className=' bg-gradient-to-r flex from-rose-500 to-transparent  pl-10 text-7xl text-black font-bold '>
-   <div className=''>
-    <img className='' 
-    alt="Challenge two" />
-    </div>
-   
-  </div>
- </section>
-  </>
-    )
- }
+import { useSearchParams } from 'next/navigation'
+
+export default function Banner() {
+  return (
+    <section className=" h-[50vw] mt-5 bg-cover bg-[url('https://www.insper.edu.br/wp-content/uploads/2021/11/tg11-Futuristic-microchip-processor-with-backlight-on-the-phone-in-blue-2.jpg')]">
+      <div className='justify-center w-full h-full to-transparent flex flex-col pl-10 text-5xl text-white font-bold '>
+        <h1 className=''>Challenge two</h1>
+        <p className='text-white p-6 text-3xl'>Aprendendo com Narue e Lilian</p>
+      </div>
+    </section>
+  )
+} 
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,25 +1,25 @@
 export default function Header() {
     return(
-        <header className='bg-black top-0 fixed w-screen flex place-items-center p-6 justify-between shadow-lg rounded-bl-lg rounded-br-lg '>
-        <h1 className='text-4xl  font-semibold text-blue-500'>My Next.js Components</h1>
+        <header className='bg-sky-500 top-0 fixed w-screen flex place-items-center p-6 justify-between shadow-lg rounded-bl-lg rounded-br-lg '>
+        <h1 className='text-4xl  font-semibold text-black'>My Next.js Components</h1>
         <nav className='flex'>
           <ul className='flex'>
-             <li className='rounded-lg px-3 py-1 text-blue-500  hover:bg-[rgba(252,250,250,0.2)] hover:scale-110 cursor-pointer transition'>
+             <li className='rounded-lg px-3 py-1 text-black  hover:bg-[rgba(104,105,107,0.2)] hover:scale-110 cursor-pointer transition'>
                 <a href="https://github.com/aryelnascimento11/project-componentes"> Git-Hub</a>
             </li>
-              <li className=' rounded-lg px-3 py-1 text-blue-500 hover:bg-[rgba(255,252,252,0.2)] hover:scale-110 cursor-pointer transition'>
+              <li className=' rounded-lg px-3 py-1 text-black hover:bg-[rgba(104,105,107,0.2)] hover:scale-110 cursor-pointer transition'>
                <a href="https://vercel.com/dashboard">Projects Vercel</a>
               </li>
-            <li className='rounded-lg px-3 py-1 text-blue-500 hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition'>
+            <li className='rounded-lg px-3 py-1 text-black hover:bg-[rgba(104,105,107,0.2)] hover:scale-110 cursor-pointer transition'>
                <a href="https://easy-portfolio-psi.vercel.app/">Portifolio</a>
             </li>
-            <li className='rounded-lg px-3 py-1 text-blue-500 hover:bg-[rgba(255,255,255,0.2)] hover:scale-110 cursor-pointer transition'>
+            <li className='rounded-lg px-3 py-1 text-black hover:bg-[rgba(104,105,107,0.2)] hover:scale-110 cursor-pointer transition'>
                <a href="https://nextjs.org/docs/app/api-reference/components">Next-Components</a>
             </li>
           </ul>
-          <button className='group/enter text-blue-500 flex gap-2 ml-10 rounded-2xl border border-blue-500 shadow-lg px-3 py-1 font-light'>Entrar
+          <button className='group/enter flex gap-2 ml-10 rounded-2xl border border-black shadow-lg px-3 py-1 font-light'>Entrar
         <path id="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"></path>
-        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" className="text-2xl group-hover/enter:text-rose-500 group-hover/enter:bg-black rounded-full transition" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"></path></svg>
+        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" className="text-2xl group-hover/enter:text-white group-hover/enter:bg-black rounded-full transition" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"></path></svg>
         </button>
         </nav>
       </header>
