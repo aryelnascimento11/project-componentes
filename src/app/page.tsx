ec24cbbcab367d8dd0a9b41daef8d58bd495fd1e
Aryel <126723967+aryelnascimento11@users.noreply.github.com>
2023-09-18 16:49:52 -0300

    props top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,22 +11,10 @@ import Exemplo from './components/exemplo'
 
 
 export default function Home() {
-
-  const usuario = {
-    nome:'aryel',
-    sobrenome:'batata',
-    senha:323121,
-    email:'exemplo@gmail.com',
-  }
-
   return (
     <>
       <Header/>
       <Banner/>
-      <p>{usuario.nome}</p>
-      <p>{usuario.sobrenome}</p>
-      <p>{usuario.senha}</p>
-      <p>{usuario.email}</p>
       <Card/>
       <Footer/>
     </>
